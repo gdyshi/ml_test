71eaa62e1bdcfd8a2dd1baeee1b6fad5d34fe6b6
gdyshi <gdyshi@126.com>
2017-12-14 17:29:08 +0800

    1

--- python/l1.py
+++ python/l1.py
@@ -9,8 +9,11 @@ def sigmoid(x):
 def sigmoid_output_to_derivative(output):
     return output * (1 - output)
 
+x_data = np.linspace(-100, 100, 10000)
+# y_data = x_data * 0.2 + 0.3 + np.random.normal(0, 0.55, x_data.size)
+y_data = x_data * 0.2 + 0.3
 
-x_dim = 2
+x_dim = 1
 y_dim = 1
 learning_rate = 0.5
 x = np.ones((x_dim, 1))
@@ -18,8 +21,7 @@ y = np.ones((y_dim, 1))
 
 l0_dim = x_dim
 l1_dim = 10
-l2_dim = 5
-l3_dim = 1
+l2_dim = 1
 
 w1 = np.random.random((l0_dim, l1_dim))
 # print('w1:' + str(w1.shape))
@@ -29,16 +31,11 @@ w2 = np.random.random((l1_dim, l2_dim))
 # print('w2:' + str(w2.shape))
 b2 = np.zeros((l2_dim, 1))
 # print('b2:' + str(b2.shape))
-w3 = np.random.random((l2_dim, l3_dim))
-# print('w3:' + str(w3.shape))
-b3 = np.zeros((l3_dim, 1))
-# print('b3:' + str(b3.shape))
 
-for i in range(2000):
+for i in range(10):
     print('i:'+ str(i))
     x[0][0] = (1)*0.01
-    x[1][0] = (1)*0.02
-    y[0][0] = (x[0][0] + x[1][0])**2
+    y[0][0] = (x[0][0])* 0.2 + 0.3
     a0 = x
     # print('a0:' + str(a0.shape))
     # def forward:
@@ -52,12 +49,7 @@ for i in range(2000):
     a2 = sigmoid(z2)
     # print('a2:' + str(z2.shape))
 
-    z3 = np.add(np.dot(w3.transpose(), a2), b3)
-    # print('z3:' + str(z3.shape))
-    a3 = sigmoid(z3)
-    # print('a3:' + str(a3.shape))
-
-    y_ = a3
+    y_ = a2
 
     cost = (y_ - y) ** 2 / 2
     # loss = cost.sum() / batch_size
@@ -66,34 +58,14 @@ for i in range(2000):
     # def backward:
     # d_loss_cost = 1 / batch_size
     # print('d_loss_cost:' + str(d_loss_cost.shape))
-    d_cost_a3 = (y_ - y)
+    d_cost_a2 = (y_ - y)
     # print('d_cost_a3:' + str(d_cost_a3.shape))
-    d_cost = d_cost_a3
+    d_cost = d_cost_a2
     # print('d_cost:' + str(d_cost.shape))
-
-    d_a3_z3 = sigmoid_output_to_derivative(z3)
-    # print('d_a3_z3:' + str(d_a3_z3.shape))
-    d_z3 = d_cost * d_a3_z3
-    # print('d_z3:' + str(d_z3.shape))
-    d_z3_b3 = np.ones_like(b3)
-    # print('d_z3_b3:' + str(d_z3_b3.shape))
-    d_b3 = d_z3*d_z3_b3
-    # print('d_b3:' + str(d_b3.shape))
-    d_z3_w3 = a2
-    # print('d_z3_w3:' + str(d_z3_w3.shape))
-    d_w3 = np.dot(d_z3_w3, d_z3.transpose())
-    # print('d_w3:' + str(d_w3.shape))
-    d_z3_a2 = w3
-    # print('d_z3_a2:' + str(d_z3_a2.shape))
-    d_a2 = d_z3_a2 * d_z3
-    # print('d_a2:' + str(d_a2.shape))
-
-    w3 -= learning_rate * d_w3
-    b3 -= learning_rate * d_b3
 
     d_a2_z2 = sigmoid_output_to_derivative(z2)
     # print('d_a2_z2:' + str(d_a2_z2.shape))
-    d_z2 = d_a2 * d_a2_z2
+    d_z2 = d_cost * d_a2_z2
     # print('d_z2:' + str(d_z2.shape))
     d_z2_b2 = np.ones_like(b2)
     # print('d_z2_b2:' + str(d_z2_b2.shape))
@@ -107,6 +79,7 @@ for i in range(2000):
     # print('d_z2_a1:' + str(d_z2_a1.shape))
     d_a1 = np.dot(d_z2_a1, d_z2)
     # print('d_a1:' + str(d_a1.shape))
+    print('w2:'+str(w2))
     w2 -= learning_rate * d_w2
     b2 -= learning_rate * d_b2
 
@@ -128,6 +101,5 @@ for i in range(2000):
     # print('d_a0:' + str(d_a0.shape))
     w1 -= learning_rate * d_w1
     b1 -= learning_rate * d_b1
-    # print('a2:'+str(a2))
-    # print('d_w3:'+str(d_w3))
-    # print('w3:'+str(w3))
+    print('a1:'+str(a1))
+    print('d_w2:'+str(d_w2))
--- python/mnist.py
+++ python/mnist.py
@@ -1,84 +1,104 @@
-import tensorflow as tf
 import numpy as np
 
-layers = [2, 20, 10, 5, 1]
-NOISE_LEVEL = 0.15
-TRAIN_DATA_NUM = 1000
-learning_rate = 0.02
+
+def sigmoid(x):
+    output = 1 / (1 + np.exp(-x))
+    return output
 
 
-# 产生双月环数据集
-def produceData(r, w, d, num):
-    r1 = r - w / 2
-    r2 = r + w / 2
-    # 上半圆
-    theta1 = np.random.uniform(0, np.pi, num)
-    X_Col1 = np.random.uniform(r1 * np.cos(theta1), r2 * np.cos(theta1), num)[:, np.newaxis]
-    X_Row1 = np.random.uniform(r1 * np.sin(theta1), r2 * np.sin(theta1), num)[:, np.newaxis]
-    Y_label1 = np.ones(num)  # 类别标签为1
-
-    # 下半圆
-    theta2 = np.random.uniform(-np.pi, 0, num)
-    X_Col2 = (np.random.uniform(r1 * np.cos(theta2), r2 * np.cos(theta2), num) + r)[:, np.newaxis]
-    X_Row2 = (np.random.uniform(r1 * np.sin(theta2), r2 * np.sin(theta2), num) - d)[:, np.newaxis]
-    Y_label2 = -np.ones(num)  # 类别标签为-1,注意：由于采取双曲正切函数作为激活函数，类别标签不能为0
-
-    # 合并
-    X_Col1 += np.random.normal(0, NOISE_LEVEL, X_Col1.shape)
-    X_Row1 += np.random.normal(0, NOISE_LEVEL, X_Row1.shape)
-    X_Col2 += np.random.normal(0, NOISE_LEVEL, X_Col2.shape)
-    X_Row2 += np.random.normal(0, NOISE_LEVEL, X_Row2.shape)
-    X_Col = np.vstack((X_Col1, X_Col2))
-    X_Row = np.vstack((X_Row1, X_Row2))
-    X = np.hstack((X_Col, X_Row))
-    Y_label = np.hstack((Y_label1, Y_label2))
-    Y_label.shape = (num * 2, 1)
-    return X, Y_label
+def sigmoid_output_to_derivative(output):
+    return output * (1 - output)
 
 
-def set_layers(x):
-    for i in range(0, len(layers) - 1):
-        X = x if i == 0 else y
-        node_in = layers[i]
-        node_out = layers[i + 1]
-        W = tf.Variable(np.random.randn(node_in, node_out).astype('float32') / (np.sqrt(node_in)))
-        b = tf.Variable(np.random.randn(node_out).astype('float32'))
-        z = tf.matmul(X, W) + b
-        y = tf.nn.tanh(z)
-    return y
+x_dim = 2
+y_dim = 1
+batch_size = 100
+learning_rate = 0.1
+X = np.ones((x_dim, batch_size))
+Y = np.ones((y_dim, batch_size))
 
+l0_dim = x_dim
+l1_dim = 10
+l2_dim = 5
+l3_dim = 1
 
-def main():
-    # 产生训练数据
-    x_data, y_label = produceData(10, 6, -6, TRAIN_DATA_NUM)
-    # 产生测试数据
-    x_test, y_test = produceData(10, 6, -6, TRAIN_DATA_NUM)
+A0 = X.reshape((l0_dim, batch_size))
+print('A0:' + str(A0.shape))
+# def forward:
+W1 = np.random.random((l0_dim, l1_dim))
+print('W1:' + str(W1.shape))
+B1 = np.zeros((l1_dim, 1))
+print('B1:' + str(B1.shape))
+Z1 = np.add(np.dot(W1.transpose(), A0), B1)
+print('Z1:' + str(Z1.shape))
+A1 = sigmoid(Z1)
+print('A1:' + str(A1.shape))
 
-    x = tf.placeholder(tf.float32, [None, 2])
-    y_ = tf.placeholder(tf.float32, [None, 1])
+W2 = np.random.random((l1_dim, l2_dim))
+print('W2:' + str(W2.shape))
+B2 = np.zeros((l2_dim, 1))
+print('B2:' + str(B2.shape))
+Z2 = np.add(np.dot(W2.transpose(), A1), B2)
+print('Z2:' + str(Z2.shape))
+A2 = sigmoid(Z2)
+print('A2:' + str(Z2.shape))
 
+W3 = np.random.random((l2_dim, l3_dim))
+print('W3:' + str(W3.shape))
+B3 = np.zeros((l3_dim, 1))
+print('B3:' + str(B3.shape))
+Z3 = np.add(np.dot(W3.transpose(), A2), B3)
+print('Z3:' + str(Z3.shape))
+A3 = sigmoid(Z3)
+print('A3:' + str(A3.shape))
 
-    y = set_layers(x)
-    # 均方误差
-    mse_loss = tf.reduce_mean(tf.reduce_sum(tf.square(y - y_), reduction_indices=[1]))
-    tf.add_to_collection('losses', mse_loss)
-    loss = tf.add_n(tf.get_collection('losses'))
-    global_step = tf.Variable(0, trainable=False)
-    rate = learning_rate
+Y_ = A3
 
-    train_step = tf.train.GradientDescentOptimizer(rate).minimize(loss, global_step=global_step)
+cost = (Y_ - Y) ** 2 / 2
+loss = cost.sum() / batch_size
+print(loss)
 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for i in range(2000):
-            train_step.run(feed_dict={x: x_data, y_: y_label})
-            if i % 1000 == 0:
-                train_loss = mse_loss.eval(feed_dict={x: x_data, y_: y_label})
-                test_loss = mse_loss.eval(feed_dict={x: x_test, y_: y_test})
-                print('step %d, 100*training loss %f,100*testing loss %f' % (i, 100 * train_loss, 100 * test_loss))
+# def backward:
+d_loss_cost = 1 / batch_size
+# print('d_loss_cost:' + str(d_loss_cost.shape))
+d_cost_A3 = (Y_ - Y)
+print('d_cost_A3:' + str(d_cost_A3.shape))
+d_cost = d_loss_cost * d_cost_A3
+print('d_cost:' + str(d_cost.shape))
 
-    print('DONE!')
+d_A3_Z3 = sigmoid_output_to_derivative(Z3)
+print('d_A3_Z3:' + str(d_A3_Z3.shape))
+d_Z3 = d_cost * d_A3_Z3
+print('d_Z3:' + str(d_Z3.shape))
+d_Z3_W3 = A2
+print('d_Z3_W3:' + str(d_Z3_W3.shape))
+d_W3 = d_Z3*d_Z3_W3
+print('d_W3:' + str(d_W3.shape))
+d_Z3_B3 = np.ones((l3_dim, batch_size))
+# print('d_Z3_B3:' + str(d_Z3_B3.shape))
+d_B3 = np.dot(d_Z3_B3, d_Z3.transpose())
+d_B3 = d_Z3*d_Z3_B3
+print('d_B3:' + str(d_B3.shape))
+d_Z3_A2 = W3
+print('d_Z3_A2:' + str(d_Z3_A2.shape))
+d_A2 = np.dot(d_Z3_A2, d_Z3)
+print('d_A2:' + str(d_A2.shape))
 
+W3 -= learning_rate * d_W3
+B3 -= learning_rate * d_B3
 
-if __name__ == '__main__':
-    main()
+d_A2_Z2 = sigmoid_output_to_derivative(Z2)
+print('d_A2_Z2:' + str(d_A2_Z2.shape))
+d_Z2 = d_A2 * d_A2_Z2
+print('d_Z2:' + str(d_Z2.shape))
+d_Z2_W2 = A1
+print('d_Z2_W2:' + str(d_Z2_W2.shape))
+d_W2 = np.dot(d_Z2_W2, d_Z2.transpose())
+print('d_W2:' + str(d_W2.shape))
+d_Z2_B2 = np.ones((l2_dim, batch_size))
+# print('d_Z2_B2:' + str(d_A3_Z3.shape))
+d_B2 = np.dot(d_Z2_B2, d_Z2.transpose())
+print('d_B2:' + str(d_B2.shape))
+W2 -= learning_rate * d_W2
+B2 -= learning_rate * d_B2
+
